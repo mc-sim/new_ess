bde8997916181155cf9631272b2e573b40b7badb
menyalin <menyalin@gmail.com>
2017-10-12 16:35:48 +0300

    add mat-toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { MatToolbarModule } from '@angular/material';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
